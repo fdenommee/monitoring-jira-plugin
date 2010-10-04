0ae9a167ed222fd0befa945928e673bcda0d9f39
François Dénommée <fdenommee@pyxis-tech.com>
2010-10-04 12:57:37 -0400

    generated skeleton

---
+++ src/main/java/com/pyxis/jira/monitoring/MyPlugin.java
@@ -0,0 +1,5 @@
+package com.pyxis.jira.monitoring;
+
+public class MyPlugin
+{
+}
---
+++ src/test/java/com/pyxis/jira/monitoring/MyPluginTest.java
@@ -0,0 +1,11 @@
+package com.pyxis.jira.monitoring;
+
+import org.junit.Test;
+
+public class MyPluginTest
+{
+    @Test
+    public void testSomething()
+    {
+    }
+}
