aeaca037d2dac524f86751450d1b00a0c84bc83c
François Dénommée <fdenommee@pyxis-tech.com>
2010-11-02 16:48:07 -0400

    Refactoring

--- src/test/java/com/pyxis/jira/monitoring/event/MonitorIssueEventListenerTest.java
+++ src/test/java/com/pyxis/jira/monitoring/event/MonitorIssueEventListenerTest.java
@@ -1,13 +1,5 @@
 package com.pyxis.jira.monitoring.event;
 
-import static com.pyxis.jira.monitoring.IssueObjectMother.TEST_1_ISSUE;
-import static com.pyxis.jira.monitoring.IssueObjectMother.TEST_2_ISSUE;
-import static com.pyxis.jira.monitoring.UserObjectMother.FDENOMMEE_USER;
-import static com.pyxis.jira.monitoring.UserObjectMother.VTHOULE_USER;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-
 import java.util.Map;
 
 import org.junit.Before;
@@ -20,7 +12,13 @@ import com.atlassian.jira.issue.IssueManager;
 import com.opensymphony.user.User;
 import com.pyxis.jira.monitoring.DefaultMonitorHelper;
 import com.pyxis.jira.monitoring.MonitorHelper;
-import com.pyxis.jira.monitoring.event.MonitorIssueEventListener;
+
+import static com.pyxis.jira.monitoring.IssueObjectMother.TEST_1_ISSUE;
+import static com.pyxis.jira.monitoring.IssueObjectMother.TEST_2_ISSUE;
+import static com.pyxis.jira.monitoring.UserObjectMother.FDENOMMEE_USER;
+import static com.pyxis.jira.monitoring.UserObjectMother.VTHOULE_USER;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class MonitorIssueEventListenerTest {
 
@@ -33,23 +31,21 @@ public class MonitorIssueEventListenerTest {
 		helper = new DefaultMonitorHelper(issueManager);
 		eventListener = new MonitorIssueEventListener(helper);
 	}
-	
+
 	@Test
 	public void shouldRemoveIssueActivityOnlyForDeletedIssue() {
 		helper.notify(FDENOMMEE_USER, TEST_1_ISSUE);
-		
+
 		helper.notify(FDENOMMEE_USER, TEST_2_ISSUE);
 		helper.notify(VTHOULE_USER, TEST_2_ISSUE);
-		
+
 		assertThat(helper.getActivities(TEST_1_ISSUE).size(), is(equalTo(1)));
 		assertThat(helper.getActivities(TEST_2_ISSUE).size(), is(equalTo(2)));
-		
+
 		IssueEvent event = new IssueEvent(TEST_1_ISSUE, (Map)null, (User)null, EventType.ISSUE_DELETED_ID);
 		eventListener.issueDeleted(event);
-		
+
 		assertThat(helper.getActivities(TEST_1_ISSUE).size(), is(equalTo(0)));
 		assertThat(helper.getActivities(TEST_2_ISSUE).size(), is(equalTo(2)));
-		
 	}
-
 }
