9e47bfb47973db7cb37b8fde4329c410168e07fa
François Dénommée <fdenommee@pyxis-tech.com>
2010-11-04 23:06:53 -0400

    Removed logger

--- src/main/java/com/pyxis/jira/monitoring/event/MonitorIssueEventRegistror.java
+++ src/main/java/com/pyxis/jira/monitoring/event/MonitorIssueEventRegistror.java
@@ -1,16 +1,10 @@
 package com.pyxis.jira.monitoring.event;
 
-import org.apache.log4j.Logger;
-
-import com.pyxis.jira.monitoring.event.MonitorIssueEventListener;
 import com.pyxis.jira.util.EventListenerUtils;
 
 public class MonitorIssueEventRegistror {
 
-	private static final Logger log = Logger.getLogger(MonitorIssueEventListener.class);
-
 	static {
-		log.fatal("Registering listener...");
 		EventListenerUtils.registerListener("Monitor Issue Event Listener", MonitorIssueEventListener.class);
 	}
 
